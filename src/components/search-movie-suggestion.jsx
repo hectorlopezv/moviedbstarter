2fb95584b08361c3fdcb6f467a3f804b7d4e336d
hector lopez <hectorvmlopez@gmail.com>
2024-06-29 17:26:27 -0500

    fix

--- src/components/search-movie-suggestion.jsx
+++ src/components/search-movie-suggestion.jsx
@@ -3,7 +3,7 @@ import Suggestion from "./suggestion";
 
 const SearchMoviesSuggestion = () => {
 
-    const { search } = useSelector((store) => store);
+    const  search  = useSelector((store) => store.search);
     const { genres } = useSelector((store) => store.genres);
     
     return (<Suggestion movies={search} genres={genres} />);
