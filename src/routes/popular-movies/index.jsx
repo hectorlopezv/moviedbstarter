d7a3bf8a35885c76d1418108ce62667f63930bf2
hector lopez <hectorvmlopez@gmail.com>
2024-06-29 17:06:59 -0500

    fix test

--- src/routes/popular-movies/index.jsx
+++ src/routes/popular-movies/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import { Typography } from "@mui/material";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { getPopularMovies, resetState } from "../../redux/movies";
